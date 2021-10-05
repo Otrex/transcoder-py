6d0679bcdb74da573b1af866463d0deb27afad4c
Treasure Obisike <treasure.obisike@chigisoft.com>
2021-10-05 14:09:53 +0100

    transcoder

--- videos.py
+++ videos.py
@@ -19,11 +19,11 @@ def download (url):
   print('--------------------------Download Complete---------------------------')
 
 videos = [
-  "lecture-contents/866bbc92-8129-49d2-86ec-73180ebfa6dc",
   "lecture-contents/5aef4f73-c79c-4f57-8abd-84cf553cb28b",
   "lecture-contents/42e2bc74-2bd0-4f35-9132-ea29a5cfab44",
   "lecture-contents/bef1a49d-5025-4d0b-9137-b910f95768f9",
   "lecture-contents/9f05a0c3-55a8-4795-960b-0d75a753e743",
+  "lecture-contents/866bbc92-8129-49d2-86ec-73180ebfa6dc",
   "lecture-contents/cb8e58d7-dbe7-44b1-a59e-218adb9512fe",
   "lecture-contents/e4e3ec62-ab03-46bb-b45a-61c2a29fcb4d",
   "lecture-contents/34b94cd6-503e-417d-8f0c-3d1e32379d63"
@@ -37,4 +37,4 @@ for url in videos:
     if os.path.exists(path):
       os.remove(path)
 
-shell("sh", 'trancoderv2.sh', downloadPath , transcodedPath )
+shell("bash", 'trancoderv2.sh', downloadPath , transcodedPath )
